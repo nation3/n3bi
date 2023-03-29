dec71ab5c7b11217470f455dc5586772ae3ba7db
aahna-ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-03-29 09:14:22 +0800

    refactor: rename to "Passport" (#90)

--- hardhat/contracts/mock/PassportMock.sol
+++ hardhat/contracts/mock/PassportMock.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Nation3GenesisPassport is ERC721 {
+contract Passport is ERC721 {
     constructor() ERC721("Nation3 Genesis Passport", "PASS3") {}
 }
