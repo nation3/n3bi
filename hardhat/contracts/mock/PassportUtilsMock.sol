8466d2a8f1478af89afc441f0902f5927629e53c
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2024-02-26 16:56:08 +0800

    ⌨️ chore: openzeppelin v5 (#113)
    close #113

--- hardhat/contracts/mock/PassportUtilsMock.sol
+++ hardhat/contracts/mock/PassportUtilsMock.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
 import "@nation3/nationcred-contracts/utils/IPassportUtils.sol";
 import "../passport/IPassportIssuer.sol";
