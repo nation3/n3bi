0c7fe3f0655c66d9e776e1a2551047dcf72b0201
TTNguyenDev <nttriet9a4@gmail.com>
2024-02-26 16:49:43 +0700

    deploy helper

--- hardhat/scripts/deploy-n3bi.ts
+++ hardhat/scripts/deploy-n3bi.ts
@@ -1,4 +1,5 @@
 import { ethers, run } from "hardhat";
+import { verifyContract, deployContract } from "./helpers"
 
 // Sepolia
 const passportUtilsAddress = "0x7Ef8C512D39547873A681242EA87881CD2b8B7B7";
@@ -10,25 +11,6 @@ const amountPerEnrollment = ethers.utils.parseEther("0.0033");
 // const nationCredAddress = "0x7794F0Eb1eA812fBcdaBD559551Fb26A79720925";
 // const amountPerEnrollment = ethers.utils.parseEther("0.0333");
 
-async function deployContract(name: string, args: Array<any>): Promise<string> {
-  const contractFactory = await ethers.getContractFactory(name);
-
-  const contract = await contractFactory.deploy(...args);
-  await contract.deployed();
-  return contract.address;
-}
-
-async function verifyContract(
-  contractPath: string,
-  contractAddress: string,
-  args: Array<any>
-) {
-  await run("verify:verify", {
-    contract: contractPath,
-    address: contractAddress,
-    constructorArguments: args,
-  });
-}
 
 async function main() {
   const contractName = "N3BI";
@@ -36,26 +18,11 @@ async function main() {
 
   // Constructor Args
   const args = [passportUtilsAddress, nationCredAddress, amountPerEnrollment];
-
   console.log("Contract is deploying....");
   const contractAddress = await deployContract(contractName, args);
-  console.log(`${contractName} deployed to: ${contractAddress}`);
-
-  console.log('Waiting for 30 seconds before verifying...');
-  await sleep(30_000);
-
-  console.log("Contract is verifying....");
   await verifyContract(contractPath, contractAddress, args);
 }
 
-function sleep(ms: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
---
+++ hardhat/scripts/helpers.ts
@@ -0,0 +1,31 @@
+import { ethers, run, network } from "hardhat";
+export async function verifyContract(contractPath: string, contractAddress: string, args: Array<any>) {
+  if (network.name == "hardhat") {
+    return
+  }
+  console.log('Waiting for 30 seconds before verifying...');
+  await sleep(30_000);
+
+  console.log('Contract is verifying....');
+  await run("verify:verify", {
+    contract: contractPath,
+    address: contractAddress,
+    constructorArguments: args,
+  });
+}
+
+export async function deployContract(name: string, args: Array<any>): Promise<string> {
+  console.log('Contract is deploying....');
+  const contractFactory = await ethers.getContractFactory(name);
+
+  const contract = await contractFactory.deploy(...args);
+  await contract.deployed();
+  console.log(`${name} deployed to: ${contract.address}`);
+  return contract.address;
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
