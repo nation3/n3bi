891685f8d7ac1a851443b2331022e0c7a393a8c6
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2024-04-15 17:54:15 +0800

    feat: enable solidity optimizer (#119)
    close #119

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -16,7 +16,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.25",
+  solidity: {
+    version: "0.8.25",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
     goerli: {
       url: process.env.GOERLI_URL || "",
