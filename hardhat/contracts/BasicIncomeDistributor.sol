ea37d5acc4ca1f7d7cabd8eb539acdc536d8dfbd
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2024-04-18 01:11:09 +0800

    refactor: remove console.log (#102)
    #102

--- hardhat/contracts/BasicIncomeDistributor.sol
+++ hardhat/contracts/BasicIncomeDistributor.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.25;
 
 import "@nation3/nationcred-contracts/INationCred.sol";
 import "@nation3/nationcred-contracts/utils/IPassportUtils.sol";
-import "hardhat/console.sol";
 
 /**
  *        ---------::::
@@ -58,10 +57,6 @@ contract BasicIncomeDistributor {
         address nationCredAddress,
         uint256 amountPerEnrollment_
     ) {
-        console.log("Deploying BasicIncomeDistributor");
-        console.log("passportUtilsAddress:", passportUtilsAddress);
-        console.log("nationCredAddress:", nationCredAddress);
-        console.log("amountPerEnrollment_:", amountPerEnrollment_);
         owner = address(msg.sender);
         passportUtils = IPassportUtils(passportUtilsAddress);
         nationCred = INationCred(nationCredAddress);
@@ -93,54 +88,40 @@ contract BasicIncomeDistributor {
 
     /// Checks if a Nation3 citizen is eligible to enroll for Basic Income.
     function isEligibleToEnroll(address citizen) public view returns (bool) {
-        console.log("isEligibleToEnroll");
-
         // The account owns the passport NFT
         if (!passportUtils.isOwner(citizen)) {
             return false;
         }
-        console.log(unicode"✅ The account owns the passport NFT");
 
         // The passport has not yet expired
         if (passportUtils.isExpired(citizen)) {
             return false;
         }
-        console.log(unicode"✅ The passport has not yet expired");
 
         // The passport is not about to expire within the next year
         uint256 expirationTimestamp = passportUtils.getExpirationTimestamp(
             citizen
         );
-        console.log("expirationTimestamp:", expirationTimestamp);
         uint256 oneYearFromNow = block.timestamp + 365 days;
-        console.log("oneYearFromNow:", oneYearFromNow);
         if (expirationTimestamp < oneYearFromNow) {
             return false;
         }
-        console.log(
-            unicode"✅ The passport is not about to expire within the next year"
-        );
 
         // The citizen is active
         if (!nationCred.isActiveAddress(citizen)) {
             return false;
         }
-        console.log(unicode"✅ The citizen is active");
 
         return true;
     }
 
     /// Once eligible, the citizen can enroll for Basic Income, as long as the smart contract contains enough funding for covering one additional citizen's Basic Income for the duration of 1 year.
     function enroll() public {
-        console.log("enroll");
-
         if (!isEligibleToEnroll(msg.sender)) {
             revert NotEligibleError(msg.sender);
         }
-        console.log(unicode"✅ The citizen is eligible for enrollment");
 
         uint256 oneYearAgo = block.timestamp - 365 days;
-        console.log("oneYearAgo:", oneYearAgo);
         if (enrollmentTimestamps[msg.sender] > oneYearAgo) {
             revert CurrentlyEnrolledError(
                 msg.sender,
@@ -149,7 +130,6 @@ contract BasicIncomeDistributor {
         }
 
         uint256 amountAvailable = address(this).balance - amountEnrolled;
-        console.log("amountAvailable:", amountAvailable);
         if (amountAvailable < amountPerEnrollment) {
             revert NotEnoughFunding(amountAvailable, amountPerEnrollment);
         }
@@ -161,35 +141,27 @@ contract BasicIncomeDistributor {
 
     /// Checks if a Nation3 citizen is eligible to claim Basic Income.
     function isEligibleToClaim(address citizen) public view returns (bool) {
-        console.log("isEligibleToClaim");
-
         // The account owns the passport NFT
         if (!passportUtils.isOwner(citizen)) {
             return false;
         }
-        console.log(unicode"✅ The account owns the passport NFT");
 
         // The passport has not yet expired
         if (passportUtils.isExpired(citizen)) {
             return false;
         }
-        console.log(unicode"✅ The passport has not yet expired");
 
         return true;
     }
 
     /// Once enrolled, citizens can claim their earned Basic Income at any time.
     function claim(address payable beneficial) public {
-        console.log("claim");
-
         if (!isEligibleToClaim(msg.sender)) {
             revert NotEligibleError(msg.sender);
         }
-        console.log(unicode"✅ The citizen is eligible for claiming");
 
         uint256 claimableAmount = getClaimableAmount(msg.sender);
         require(claimableAmount > 0, "There is no reward to claim.");
-        console.log("claimableAmount:", claimableAmount);
 
         // Transfer token to recipient
         beneficial.transfer(claimableAmount);
--- hardhat/contracts/mock/VotingEscrowMock.sol
+++ hardhat/contracts/mock/VotingEscrowMock.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.25;
 
 import "../governance/IVotingEscrow.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "hardhat/console.sol";
 
 contract VotingEscrowMock is ERC20, IVotingEscrow {
     mapping(address => LockedBalance) locks;
@@ -13,9 +12,6 @@ contract VotingEscrowMock is ERC20, IVotingEscrow {
     }
 
     function create_lock(int128 _value, uint256 _unlock_time) public {
-        console.log("create_lock");
-        console.log("uint256(int256(_value)):", uint256(int256(_value)));
-        console.log("_unlock_time:", _unlock_time);
         LockedBalance memory lockedBalance = LockedBalance({
             amount: _value,
             end: _unlock_time
