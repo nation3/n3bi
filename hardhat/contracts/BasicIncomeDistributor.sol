2832fe08cd146e306844265e5464cf3a195ef1c4
x-dev-private <gabrielcostaribeiro666@gmail.com>
2024-06-02 13:45:50 -0300

    solidity updated to 0.8.26

--- hardhat/contracts/BasicIncomeDistributor.sol
+++ hardhat/contracts/BasicIncomeDistributor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.26;
 
 import "@nation3/nationcred-contracts/INationCred.sol";
 import "@nation3/nationcred-contracts/utils/IPassportUtils.sol";
