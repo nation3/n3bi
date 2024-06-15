a5813a7cbac9ba7408bb35cf3b9694c246c743dc
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2024-06-15 21:48:51 +0700

    Update hardhat/scripts/deploy-distributor.ts

--- hardhat/scripts/deploy-distributor.ts
+++ hardhat/scripts/deploy-distributor.ts
@@ -12,7 +12,7 @@ const ownerAddress = "0xFBB66BC799308435eD2a0e0C0ac3ad1D46749B7b";
 // const passportUtilsAddress = "0x23Ca3002706b71a440860E3cf8ff64679A00C9d7";
 // const nationCredAddress = "0x7794F0Eb1eA812fBcdaBD559551Fb26A79720925";
 // const amountPerEnrollment = ethers.utils.parseEther("0.0333");
-// const ownerAddress = "0x5afEb7F3259A25EB21287e3A917BeE3d4dE58dAf"
+// const ownerAddress = "0x5afEb7F3259A25EB21287e3A917BeE3d4dE58dAf";
 
 
 async function main() {
