b49d8dc9fceee4fade0956dd587db4a86e02d441
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2024-08-14 16:43:07 +0700

    🌈 feat(ui): install rainbowkit
    #108

--- ui/src/pages/_app.tsx
+++ ui/src/pages/_app.tsx
@@ -1,29 +1,32 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Poppins } from 'next/font/google'
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
 
-import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
-import { publicProvider } from 'wagmi/providers/public'
- 
-const { chains, publicClient } = configureChains(
-  [mainnet],
-  [publicProvider()]
-)
-
-const config = createConfig({
-  autoConnect: true,
-  publicClient
+const config = getDefaultConfig({
+  appName: 'Nation3 Basic Income',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [mainnet, sepolia]
 })
+
+const queryClient = new QueryClient()
 
 const poppins = Poppins({ subsets: ['latin'], weight: '400' })
 
 export default function App({ Component, pageProps }: AppProps) {
   console.info('App')
   return (
-    <WagmiConfig config={config}>
-      <main className={poppins.className}>
-        <Component {...pageProps} />
-      </main>
-    </WagmiConfig>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <main className={poppins.className}>
+            <Component {...pageProps} />
+          </main>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 }
